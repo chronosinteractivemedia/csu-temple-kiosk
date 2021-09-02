6ff42e95a9cddaa1a7dbb3ac9f4f50707068a470
Jordan Sullivan <jordanmsllvn@gmail.com>
2021-09-02 17:10:28 -0600

    fixing image urls for staging site

--- renderer/config.js
+++ renderer/config.js
@@ -1,2 +1,14 @@
+function getImageUrl(apiUrl){
+    if(
+        (typeof process !== 'undefined' && process.env && process.env.IS_SERVER) ||
+        (typeof window !== 'undefined' && window.location.host.indexOf("csu-kiosk-stage.onrender.com") > -1) || 
+        (typeof window !== 'undefined' && window.location.host.indexOf("localhost:3000") > -1)
+    ){
+        return apiUrl;
+    } else {
+        return '';
+    }
+}
+
 export const apiUrl = 'https://csu-tg-cms.onrender.com';
-export const imgUrl = '';
+export const imgUrl = getImageUrl(apiUrl);
